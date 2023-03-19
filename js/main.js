b1a836bdb26e68cf8bc4b8f8de32c0e3addffd77
Shere1-1Khan <125498565+Shere1-1Khan@users.noreply.github.com>
2023-03-19 21:21:18 +0300

    Update js/main.js
    Co-authored-by: Ivan Dolgov <NightDiRaven@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {createPhotos} from './mockPhotos.js';
 import {renderPhotos} from './renderingPicture.js';
-import { addHandler } from './popupGallery.js';
+import { setPhotos} from './popupGallery.js';
 
 const photos = createPhotos();
 renderPhotos(photos);
