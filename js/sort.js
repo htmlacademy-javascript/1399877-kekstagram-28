d3b0a42873dc9531287dce25ae279003e225fa2c
Ткаченко Игорь <i.tkachenko6991@gmail.com>
2023-04-09 19:33:56 +0300

    add class active

--- js/sort.js
+++ js/sort.js
@@ -5,17 +5,27 @@ const initSubmitSort = (arr, render)=>{
   const filterRandom = document.querySelector('#filter-random');
   const filterDefault = document.querySelector('#filter-default');
   const cloneArr = arr.slice();
+  let buttonActive = document.querySelector('#filter-default');
 
-  const sortByPopularity = () => {
-
+  const addClassActive = (evt) =>{
+    buttonActive = evt.target;
+    buttonActive.classList.add('img-filters__button--active');
+  };
+  const removeClassAtive = () =>{
+    buttonActive.classList.remove('img-filters__button--active');
+  };
+  const sortByPopularity = (evt) => {
+    removeClassAtive();
+    addClassActive(evt);
     cloneArr.sort((a, b) => b.comments.length - a.comments.length);
     render(cloneArr);
 
   };
 
-  const sortRandomElements = ()=>{
+  const sortRandomElements = (evt)=>{
     const sortArr = [];
-
+    removeClassAtive();
+    addClassActive(evt);
     while(sortArr.length !== 10){
       if(new Set(sortArr).size === sortArr.length){
         sortArr.push(cloneArr[getRandomPositiveInteger(0, cloneArr.length - 1)]);
@@ -27,7 +37,11 @@ const initSubmitSort = (arr, render)=>{
 
   };
 
-  const sortByDefault = () => render(arr);
+  const sortByDefault = (evt) => {
+    removeClassAtive();
+    addClassActive(evt);
+    render(arr);
+  };
 
   filterDiscussed.addEventListener('click', sortByPopularity);
   filterRandom.addEventListener('click',sortRandomElements);
